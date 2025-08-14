de7c9980adbee403e76cfc0a32502982908307c6
h7ml <h7ml@qq.com>
2025-08-14 20:38:39 +0800

    build(deps): 降级 Fresh 并设置构建目标
    - 将 Fresh 降级到 2.0.0-alpha.34 版本
    - 设置构建目标为 Chrome 99、Firefox 99 和 Safari 15

--- fresh.config.ts
+++ fresh.config.ts
@@ -3,4 +3,7 @@ import tailwind from "$fresh/plugins/tailwind.ts";
 
 export default defineConfig({
   plugins: [tailwind()],
+  build: {
+    target: ["chrome99", "firefox99", "safari15"],
+  },
 });
